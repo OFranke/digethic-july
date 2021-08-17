66864809ebf103cb694d7f32e7b27e0d653b4e14
Oliver Franke <oliver.franke@limebit.de>
2021-08-17 12:04:04 +0200

    DIG-98 save model as pickle file

--- train.py
+++ train.py
@@ -1,12 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import numpy as np
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import KFold, cross_val_score
-from sklearn.metrics import classification_report, confusion_matrix  
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.metrics import confusion_matrix  
+import pickle
 
 #List with attribute names (it is optional to do this but it gives a better understanding of the data for a human reader)
 attribute_names = ['variance_wavelet_transformed_image', 'skewness_wavelet_transformed_image', 'curtosis_wavelet_transformed_image', 'entropy_image', 'class']
@@ -41,10 +37,5 @@ classifier = DecisionTreeClassifier()
 
 classifier = classifier.fit(x_train, y_train) 
 
-y_pred = classifier.predict(x_test) 
-
-conf_mat = confusion_matrix(y_test, y_pred)
-
-accuracy = (conf_mat[0,0] + conf_mat[1,1]) /(conf_mat[0,0]+conf_mat[0,1]+ conf_mat[1,0]+conf_mat[1,1])
-
-print("accuracy", accuracy)
+file_to_write = open("models/baummethoden.pickle", "wb")
+pickle.dump(classifier, file_to_write)
