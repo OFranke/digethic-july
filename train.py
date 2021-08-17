0181c5389949a88322337318176f8101d1db6d2a
Oliver Franke <oliver.franke@limebit.de>
2021-08-17 11:50:20 +0200

    DIG-97 add model training

--- train.py
+++ train.py
@@ -1,5 +1,12 @@
-#Import Pandas library
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.metrics import classification_report, confusion_matrix  
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
 
 #List with attribute names (it is optional to do this but it gives a better understanding of the data for a human reader)
 attribute_names = ['variance_wavelet_transformed_image', 'skewness_wavelet_transformed_image', 'curtosis_wavelet_transformed_image', 'entropy_image', 'class']
@@ -10,24 +17,17 @@ data = pd.read_csv('data/data_banknote_authentication.csv', names=attribute_name
 #Shuffle data
 data = data.sample(frac=1)
 
-#Shows the first 5 rows of the data
-data.head()
-
 #Get the absolute number of how many instances in our data belong to class zero
 count_real = len(data.loc[data['class']==0])
-print('Real bills absolute: ' + str(count_real))
 
 #Get the absolute number of how many instances in our data belong to class one
 count_fake = len(data.loc[data['class']==1])
-print('Fake bills abolute: ' +str(count_fake))
 
 #Get the relative number of how many instances in our data belong to class zero
 percentage_real = count_real/(count_fake+count_real)
-print('Real bills in percent: ' + str(round(percentage_real,3)))
 
 #Get the relative number of how many instances in our data belong to class one
 percentage_fake = count_fake/(count_real+count_fake)
-print('Fake bills in percent: ' + str(round(percentage_fake,3)))
 
 #'class'-column
 y_variable = data['class']
@@ -35,111 +35,16 @@ y_variable = data['class']
 #all columns that are not the 'class'-column -> all columns that contain the attributes
 x_variables = data.loc[:, data.columns != 'class']
 
-#import method from sklearn to split our data into training and test data
-from sklearn.model_selection import train_test_split
-import numpy as np
-
-#splits into training and test data
 x_train, x_test, y_train, y_test = train_test_split(x_variables, y_variable, test_size=0.2)
 
-# shapes of our data splits
-print(x_train.shape) 
-print(x_test.shape)
-print(y_train.shape)
-print(y_test.shape)
-
-#import DecisionTreeClassifier from the Sklearn library
-from sklearn.tree import DecisionTreeClassifier
-
-#Create a classifier object 
 classifier = DecisionTreeClassifier() 
 
-#Classfier builds Decision Tree with training data
 classifier = classifier.fit(x_train, y_train) 
 
-#Shows importances of the attributes according to our model 
-classifier.feature_importances_
-
-from sklearn.tree import export_graphviz
-
-#Export as dot file
-export_graphviz(classifier, out_file='tree_bills.dot', class_names = ['0','1'], feature_names = attribute_names[0:4])
-
-#Export dot to png 
-from subprocess import check_call
-check_call(['dot','-Tpng','tree_bills.dot','-o','tree_bills.png'])
-
-#Get predicted values from test data 
 y_pred = classifier.predict(x_test) 
 
-from sklearn.metrics import classification_report, confusion_matrix  
-
-#Create the matrix that shows how often predicitons were done correctly and how often theey failed.
 conf_mat = confusion_matrix(y_test, y_pred)
 
-#The diagonal ones are the correctly predicted instances. The sum of this number devided by the number of all instances gives us the accuracy in percent.
 accuracy = (conf_mat[0,0] + conf_mat[1,1]) /(conf_mat[0,0]+conf_mat[0,1]+ conf_mat[1,0]+conf_mat[1,1])
 
-print('Accuracy: ' + str(round(accuracy,4)))
-print('Confusion matrix:')
-print(conf_mat)  
-print('classification report:')
-print(classification_report(y_test, y_pred)) 
-
-from sklearn.model_selection import KFold, cross_val_score
-
-#k_fold object
-k_fold = KFold(n_splits=5, shuffle=True, random_state=0)
-
-#scores reached with different splits of training/test data 
-k_fold_scores = cross_val_score(classifier, x_variables, y_variable, cv=k_fold, n_jobs=1)
-
-#arithmetic mean of accuracy scores 
-mean_accuracy = np.mean(k_fold_scores)
-
-print(round(mean_accuracy, 4))
-
-from sklearn.model_selection import GridSearchCV
-
-#tree parameters which shall be tested
-tree_para = {'criterion':['gini','entropy'],'max_depth':[i for i in range(1,20)], 'min_samples_split':[i for i in range (2,20)]}
-
-#GridSearchCV object
-grd_clf = GridSearchCV(classifier, tree_para, cv=5)
-
-#creates differnt trees with all the differnet parameters out of our data
-grd_clf.fit(x_variables, y_variable)
-
-#best paramters that were found
-best_parameters = grd_clf.best_params_  
-print(best_parameters)  
-
-#new tree object with best parameters
-model_with_best_tree_parameters = grd_clf.best_estimator_
-
-#k_fold object
-k_fold = KFold(n_splits=5, shuffle=True, random_state=0)
-
-#scores reached with different splits of training/test data 
-k_fold_scores = cross_val_score(model_with_best_tree_parameters, x_variables, y_variable, cv=k_fold, n_jobs=1)
-
-#arithmetic mean of accuracy scores 
-mean_accuracy_best_parameters_tree = np.mean(k_fold_scores)
-
-print(round(mean_accuracy_best_parameters_tree, 4))
-
-
-from sklearn.ensemble import RandomForestClassifier
-
-#RandomForestClassifier object
-random_forest_classifier = RandomForestClassifier(n_estimators=10)
-
-#list with accuracies with different test and training sets of Random Forest
-accuracies_rand_forest = cross_val_score(random_forest_classifier, x_variables, y_variable, cv=k_fold, n_jobs=1)
-
-##arithmetic mean of the list with the accuracies of the Random Forest
-accuracy_rand = np.mean(accuracies_rand_forest)
-
-print('Accuracy Random Forest ' + str(round(accuracy_rand,4)))
-print('Old accuracy: ' + str(round(mean_accuracy,4)))
-print('Best tree accuracy: ' + str(round(mean_accuracy_best_parameters_tree,4)))
+print("accuracy", accuracy)
